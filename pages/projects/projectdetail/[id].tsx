759c9ac58107c4d0371abd22f0474ebd23bdf9e1
Abdul Muqeet <39890909+abdulmuqeet1@users.noreply.github.com>
2021-09-03 06:48:30 +0500

    project page fix

---
+++ pages/projects/projectdetail/[id].tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projectdetail = () => {
+  return (
+    <div>
+      <p>Project Detail Page - coming Soon... </p>
+    </div>
+  );
+};
+
+export default Projectdetail;
