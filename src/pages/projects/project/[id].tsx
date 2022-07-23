76991d4840d28133fbf39cbcba4d899d413cb430
abdulmuqeet1 <abdulmuqeet521@gmail.com>
2022-07-23 18:48:29 +0500

    proj detail page

--- src/pages/projects/project/[id].tsx
+++ src/pages/projects/project/[id].tsx
@@ -1,9 +1,34 @@
 import type { NextPage } from "next";
-// Styles
-// import styles from "../../styles/projects.module.scss";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../../../styles/projects.module.scss";
 
-const Project: NextPage = () => {
-  return <div>Project Detail Page</div>;
+import { projectList } from "../../../dataSource/";
+
+const ProjectDetail: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const projData = projectList.filter((item) => item.projectID == id)[0];
+  if (projData) {
+    return (
+      <div className={styles.root}>
+        <div className={styles.imgBox}>
+          <Image
+            src={"/assets/projectDesign.jpg"}
+            alt={projData.title}
+            layout="fill"
+          />
+        </div>
+        <div className={styles.detailBox}>
+          <h2>{projData.title}</h2>
+          <p>{projData.detail}</p>
+        </div>
+      </div>
+    );
+  } else {
+    // replace below div with a loader
+    return <div>project not found!</div>;
+  }
 };
 
-export default Project;
+export default ProjectDetail;
