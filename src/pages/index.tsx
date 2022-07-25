f2a773c6a3aca1a726c5f97459e4e68576455e1a
abdulmuqeet1 <abdulmuqeet521@gmail.com>
2022-07-25 21:00:01 +0500

    projList comp dynamic import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import type { NextPage } from "next";
+import React, { Suspense } from "react";
+import dynamic from "next/dynamic";
 // components
 import HeroHeader from "../components/compound/heroHeader";
-import HomeProjList from "../components/compound/homeProjListComp";
+import CustomLoader from "../components/atom/loader";
 // Styles
 import styles from "../styles/Home.module.scss";
+
+const DynamicProjectList = dynamic(
+  () => import("../components/compound/homeProjListComp"),
+  {
+    suspense: true,
+  }
+);
 
 const Home: NextPage = () => {
   return (
     <div>
       <main className={styles.main}>
         <HeroHeader />
-        <HomeProjList />
+        <Suspense fallback={<CustomLoader />}>
+          <DynamicProjectList />
+        </Suspense>
       </main>
     </div>
   );
