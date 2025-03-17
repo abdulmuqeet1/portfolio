3904a244196e4d52f207b44d0365fe037b17ff97
abdulmuqeet1 <abdulmuqeet521@gmail.com>
2025-03-17 17:22:40 +0500

    global store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
 import 'styles/App.scss';
 
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const count = useSelector((state: any) => state.counter.value);
+  console.log({ count });
   return <div>portfolio appss</div>;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import { BrowserRouter } from 'react-router';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRoutes from './components/router';
+import store from '@/store';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppRoutes />
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
